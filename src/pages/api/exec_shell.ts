34b59a9f4768a584a7158872acae2047c4ee08e3
LeoDAJM <diego.jimenez.m.2001@gmail.com>
2024-11-14 00:49:17 -0400

    dax

--- src/pages/api/exec_shell.ts
+++ src/pages/api/exec_shell.ts
@@ -12,38 +12,17 @@ import { spawn } from 'child_process';
 export async function post({ request }) {
   // Aquí recibimos la petición POST
   try {
-    const { args } = await request.json(); // Si necesitas recibir datos
-
+    const arg = await request.text();
+    //const { args } = await request.json(); // Si necesitas recibir datos
+    const { stdout, stderr } = await exec.promise(`sh ./scripts/mi-script.sh ${arg}`);
     // Ejecutamos el script .sh
-    const result = await new Promise((resolve, reject) => {
-      const script = spawn("sh ./src/scripts/ping.sh $args"); // Asumiendo que el script está en ./scripts/
-
-      let output = "";
-      script.stdout.on("data", (data) => {
-        output += data.toString(); // Guardamos la salida
-      });
-
-      script.stderr.on("data", (data) => {
-        console.error(data.toString()); // Capturamos errores
-      });
-
-      script.on("close", (code) => {
-        if (code === 0) {
-          resolve(output); // Respondemos con la salida si no hay errores
-        } else {
-          reject(new Error("Error al ejecutar el script, código de salida: ${code}"));
-        }
-      });
-    });
-
-    // Respondemos con el resultado
-    return new Response(JSON.stringify({ success: true, message: result }), {
+    return new Response(JSON.stringify({ success: true, message: stdout }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
 
   } catch (error) {
-    // En caso de error, respondemos con un error
+    // En caso de error en la ejecución del script
     return new Response(JSON.stringify({ success: false, message: error.message }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
